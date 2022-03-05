eb53845a5bff11672b7aaabf392394013e49142a
Ernest  Awangya <ernest.awangya@gmail.com>
2022-03-05 10:36:09 -0700

    gfhj

--- terraform/1-provider.tf
+++ terraform/1-provider.tf
@@ -1,12 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.4.0"
-    }
-  }
-}
-
 provider "aws" {
  region = "us-east-2"
+ access_key = "AKIAQN3LPLSTH23OU4PO"
+ secret_key = "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"
 }
